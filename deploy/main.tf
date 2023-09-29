489ba334ff7940cabfa51dcb25d5efb7622bf5dc
kmng <kmng@ucla.edu>
2023-09-29 11:34:13 -0700

    add cloudwatch tragger to notification

--- deploy/main.tf
+++ deploy/main.tf
@@ -688,3 +688,72 @@ resource "aws_lambda_function" "pipeline_notification_function" {
     }
   }
 }
+
+resource "aws_cloudwatch_event_rule" "lambda_trigger_rule" {
+  name        = "lambda-trigger-rule"
+  description = "Trigger Lambda function on event"
+  event_pattern = <<EOF
+  {
+    "source": ["aws.codepipeline"],
+    "detail-type": ["CodePipeline Stage Execution State Change"],
+    "resources": ["arn:aws:codepipeline:*"],
+    "detail": {
+      "state": ["SUCCEEDED"]
+    }
+  }
+  EOF
+}
+
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_trigger_rule.name
+  target_id = "target-lambda-function"
+
+  role_arn  = aws_iam_role.cloudwatch_events_role.arn
+
+  arn = aws_lambda_function.pipeline_notification_function.arn
+}
+
+
+resource "aws_iam_role" "cloudwatch_events_role" {
+  name = "cloudwatch-events-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_execution_policy" {
+  name        = "lambda-execution-policy"
+  description = "Policy for Lambda execution from CloudWatch Events"
+  policy      = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "lambda:InvokeFunction"
+        ],
+        "Resource": [
+          "${aws_lambda_function.pipeline_notification_function.arn}"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+  role       = aws_iam_role.cloudwatch_events_role.name
+}
